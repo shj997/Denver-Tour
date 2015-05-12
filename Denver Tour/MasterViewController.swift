64de259bc0ab2739fc699d260d3a737b0c36fe03
Molly <Molly@Mollys-MacBook-Pro.local>
2015-05-12 13:29:59 -0500

    Denver Travel App
    A simple app for Denver Tourism

--- Denver Tour/MasterViewController.swift
+++ Denver Tour/MasterViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class MasterViewController: UITableViewController {
 
-    var objects = [AnyObject]()
+    var things = [MyWhatsit]()
 
 
     override func awakeFromNib() {
@@ -32,9 +32,9 @@ class MasterViewController: UITableViewController {
     }
 
     func insertNewObject(sender: AnyObject) {
-        objects.insert(NSDate(), atIndex: 0)
+        things.insert(NSDate(), atIndex: 0)
         let indexPath = NSIndexPath(forRow: 0, inSection: 0)
-        self.tableView.insertRowsAtIndexPaths([indexPath], withRowAnimation: .Automatic)
+         self.tableView.insertRowsAtIndexPaths([indexPath], withRowAnimation: .Automatic)
     }
 
     // MARK: - Segues
@@ -42,7 +42,7 @@ class MasterViewController: UITableViewController {
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         if segue.identifier == "showDetail" {
             if let indexPath = self.tableView.indexPathForSelectedRow() {
-                let object = objects[indexPath.row] as NSDate
+                let object = things[indexPath.row] as NSDate
             (segue.destinationViewController as DetailViewController).detailItem = object
             }
         }
@@ -55,13 +55,13 @@ class MasterViewController: UITableViewController {
     }
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return objects.count
+        return things.count
     }
 
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath) as UITableViewCell
 
-        let object = objects[indexPath.row] as NSDate
+        let object = things[indexPath.row] as NSDate
         cell.textLabel!.text = object.description
         return cell
     }
@@ -73,7 +73,7 @@ class MasterViewController: UITableViewController {
 
     override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
         if editingStyle == .Delete {
-            objects.removeAtIndex(indexPath.row)
+            things.removeAtIndex(indexPath.row)
             tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
         } else if editingStyle == .Insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view.
---
+++ Denver Tour/Mywhatsit.swift
@@ -0,0 +1,20 @@
+//
+//  Mywhatsit.swift
+//  Denver Tour
+//
+//  Created by Molly on 4/2/15.
+//  Copyright (c) 2015 Molly. All rights reserved.
+//
+
+import Foundation
+
+class MyWhatsit {
+    var name: String
+    var location: String
+    
+    init( name: String, location: String = "" ) {
+        self.name = name;
+        self.location = location
+    }
+    
+}
